ca919a1d30a18cf7a89b7bae5242bea0befca0ef
Kenji Saito <ksaito@do-johodai.ac.jp>
2024-05-08 19:14:48 +0900

    新規ユーザ登録部分を作成中。あとRegisterGoogleHandler.tsに少しコードの追加が必要。

---
+++ src/identity/interaction/google/RegisterGoogleHandler.ts
@@ -0,0 +1,101 @@
+import { object, string } from 'yup';
+import { getLoggerFor } from '@solid/community-server';
+import type { AccountStore } from '@solid/community-server';
+import type { CookieStore } from '@solid/community-server';
+import type { JsonRepresentation } from '@solid/community-server';
+import type { JsonInteractionHandlerInput } from '@solid/community-server';
+import type { JsonView } from '@solid/community-server';
+import type { LoginOutputType } from '@solid/community-server';
+import { ResolveLoginHandler } from '@solid/community-server';
+import { parseSchema, URL_SCHEMA, validateWithError } from '@solid/community-server';
+import { GoogleStore } from './util/GoogleStore';
+import { SOLID_META } from '@solid/community-server';
+import { DataFactory } from 'n3';
+import namedNode = DataFactory.namedNode;
+import { GSessionStore } from './util/GSessionStore';
+import { GoogleIdRoute } from './util/GoogleIdRoute';
+import { GoogleOIDC, CGA } from './GoogleOIDC';
+
+const inSchema = object({
+  url:string().required(),
+});
+
+type OutType = { result: string };
+
+export interface RegisterGoogleHandlerArgs {
+  googleOIDC: GoogleOIDC;
+  accountStore: AccountStore;
+  googleRoute: GoogleIdRoute;
+  googleStore: GoogleStore;
+  gSessionStore: GSessionStore;
+  cookieStore: CookieStore;
+}
+
+/**
+ * controls.account.create,controls.google.create,controls.google.loginの3つの
+ * を合体させた処理を、このRegisterGoogleHandlerで実現する。
+ */
+export class RegisterGoogleHandler extends ResolveLoginHandler implements JsonView {
+  protected readonly logger = getLoggerFor(this);
+
+  private readonly googleOIDC: GoogleOIDC;
+  private readonly googleStore: GoogleStore;
+  private readonly googleRoute: GoogleIdRoute;
+  private readonly gSessionStore: GSessionStore;
+
+  public constructor(args: RegisterGoogleHandlerArgs) {
+    super(args.accountStore, args.cookieStore);
+    this.googleOIDC = args.googleOIDC;
+    this.googleStore = args.googleStore;
+    this.googleRoute = args.googleRoute;
+    this.gSessionStore = args.gSessionStore;
+  }
+
+  public async getView(args: JsonInteractionHandlerInput): Promise<JsonRepresentation> {
+    const { accountId, json, metadata } = args;
+    const cookie = metadata.get(namedNode('urn:npm:solid:community-server:http:accountCookie'),SOLID_META.ResponseMetadata)?.value;
+    if (!cookie) {
+      throw new Error('RegisterGoogleHandler: no cookie.');
+    }
+    const { code_verifier, code_challenge } = this.googleOIDC.createCode();
+    this.gSessionStore.set(cookie,'code_verifier',code_verifier);
+    const redirect_url = args.target; // http://localhost:3000/.account/login/google/ のはず
+    const params = {
+      scope: 'openid email  profile',
+      code_challenge,
+      code_challenge_method: 'S256',
+      redirect_url
+    };
+    const goToUrl = this.googleOIDC.client.authorizationUrl(params);
+    return { json: { ...parseSchema(inSchema), goToUrl }};
+  }
+
+  // 通常のResolveLoginHandlerのhandleメソッドの一番最初で以下のloginが呼び出される。
+  // returnでaccountIdを返せば、あとのログイン操作はResolveLoginHandlerのhandleの残りの
+  // プログラムがやってくれる。
+  public async login(args: JsonInteractionHandlerInput): Promise<JsonRepresentation<LoginOutputType>> {
+    const { json, metadata } = args;
+    const { url } = await validateWithError(inSchema, json);
+    const cookie = metadata.get(CGA.terms.cgaCookie)?.value;
+    if (!cookie) {
+      throw new Error('RegisterGoogleHandler: no cookie.');
+    }
+    const code_verifier = await this.gSessionStore.get(cookie,'code_verifier');
+
+    let sub = 'dummy';
+    try {
+      const queries = this.googleOIDC.client.callbackParams(url);
+      const callbackUrl = 'http://localhost:3000/.account/google/oidc/'; // GAHA: 動的に入手する方法？
+      const tokenSet = await this.googleOIDC.client.callback(callbackUrl,queries,{ code_verifier });
+      const claims = tokenSet.claims();
+      sub = claims.sub;
+      this.gSessionStore.delete(cookie,'code_verifier');
+    } catch(err) {
+      console.log("RegisterGoogleHandler: err=",err);
+    }
+    // GAHA GAHA GAHA GAHA GAHA GAHA GAHA GAHA GAHA GAHA GAHA
+    // これ以下の部分がまだ未完成
+    const { accountId } = await this.googleStore.authenticate(sub);
+    return { json: { accountId, remember: false }};
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './identity/interaction/google/GoogleOIDCHandler';
 export * from './identity/interaction/google/CreateGoogleHandler';
 export * from './identity/interaction/google/GoogleLoginHandler';
 export * from './identity/interaction/google/DeleteGoogleHandler';
+export * from './identity/interaction/google/RegisterGoogleHandler';
 export * from './identity/interaction/google/util/GoogleIdRoute';
 export * from './identity/interaction/google/util/GoogleStore';
 export * from './identity/interaction/google/util/BaseGoogleStore';
